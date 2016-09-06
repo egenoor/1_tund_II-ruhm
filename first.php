713857f99c974dab5f789165afb1754eb17ac6c1
egenoor <egenoor@greeny.cs.tlu.ee>
2016-09-06 15:17:15 +0300

    first.php

---
+++ first.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+	$firstName = "Ege";
+	$lastName = "Noor";
+	
+	// echo trukib valja muutuja veebilehel
+	
+	//echo $firstName;
+	//echo " ";
+	//echo $lastName;
+	
+	// punkt liidab stringe 
+	
+	echo $firstName. " ".$lastName;
+
+?>
+
